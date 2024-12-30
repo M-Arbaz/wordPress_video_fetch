82b045d29b5cf24a3d4de6f45f7343dc4935fd09
Muhammad Arbaz <28670muhammadarbaz@gmail.com>
2024-12-30 14:49:49 +0500

    youtube video thumbnail fetch from page by ID

--- function.php
+++ function.php
@@ -1,6 +1,6 @@
 <php?
 function get_first_five_video_thumbnails() {
-    $documentory_page_id = 1191; 
+    $documentory_page_id = "page id"; 
     $content = get_post_field('post_content', $documentory_page_id);
 
     if (preg_match_all('/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/', $content, $matches)) {
@@ -9,14 +9,14 @@ function get_first_five_video_thumbnails() {
 
         for ($i = 0; $i < min(6, count($video_ids)); $i++) {
             $video_id = $video_ids[$i];
-            $thumbnail_url = "https://img.youtube.com/vi/$video_id/0.jpg"; // Generate thumbnail URL
+            $thumbnail_url = "https://img.youtube.com/vi/$video_id/0.jpg"; 
 
             $output .= '<a href="' . get_permalink($documentory_page_id) . '">
                             <img src="' . esc_url($thumbnail_url) . '" alt="Documentory Video Thumbnail">
                         </a>';
         }
 
-        return $output; // Return the thumbnails
+        return $output; 
     }
 
     return 'No videos found.';
