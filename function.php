1d1227002c3ac2492b330d2b1cba6d2cab8e7c1f
Muhammad Arbaz <28670muhammadarbaz@gmail.com>
2024-12-30 14:44:07 +0500

    fetch function thumbnails

---
+++ function.php
@@ -0,0 +1,24 @@
+<php?
+function get_first_five_video_thumbnails() {
+    $documentory_page_id = 1191; 
+    $content = get_post_field('post_content', $documentory_page_id);
+
+    if (preg_match_all('/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/', $content, $matches)) {
+        $video_ids = $matches[1]; 
+        $output = '';
+
+        for ($i = 0; $i < min(6, count($video_ids)); $i++) {
+            $video_id = $video_ids[$i];
+            $thumbnail_url = "https://img.youtube.com/vi/$video_id/0.jpg"; // Generate thumbnail URL
+
+            $output .= '<a href="' . get_permalink($documentory_page_id) . '">
+                            <img src="' . esc_url($thumbnail_url) . '" alt="Documentory Video Thumbnail">
+                        </a>';
+        }
+
+        return $output; // Return the thumbnails
+    }
+
+    return 'No videos found.';
+}
+add_shortcode('first_five_video_thumbnails', 'get_first_five_video_thumbnails');
